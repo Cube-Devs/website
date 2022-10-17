5e1486736e2c1a64cc14acb941a195d81b8afb97
Kamil B <kamil.baraniak5@gmail.com>
2022-10-17 21:08:19 +0000

    fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const c = "https://bitbucket.org/cube-devs/cubevalleyclient/get/d6f3867c6e972a46a56ce94dc17246874994545c.zip";
-const s = "https://bitbucket.org/cube-devs/cubevalleyserver/get/a034b1ea905fb0947201fe5933f79f94513fe609.zip";\
+const s = "https://bitbucket.org/cube-devs/cubevalleyserver/get/a034b1ea905fb0947201fe5933f79f94513fe609.zip";
 const sc = "https://bitbucket.org/cube-devs/workspace/projects/CUB2";
 
 function download(url){
