20acf05ad1e0b842ec702be530fee4fa0eda5678
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2022-12-08 09:07:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const c = "https://github.com/Cube-Devs/cube-valley-1.16.5-server/archive/refs/heads/main.zip";
 const s = "https://github.com/Cube-Devs/cube-valley-1.16.5-client/archive/refs/heads/main.zip";
-const sc = "https://github.com/Cube-Devs/";
+const sc = "https://github.com/orgs/Cube-Devs/repositories";
 
 function download(url){
     window.open(url);
