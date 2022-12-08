a869a67c08e34144edd65186c53a10499aa6d106
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2022-12-08 09:04:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const c = "https://bitbucket.org/cube-devs/cubevalleyclient/get/d6f3867c6e972a46a56ce94dc17246874994545c.zip";
-const s = "https://bitbucket.org/cube-devs/cubevalleyserver/get/a034b1ea905fb0947201fe5933f79f94513fe609.zip";
-const sc = "https://bitbucket.org/cube-devs/";
+const c = "https://github.com/Cube-Devs/cube-valley-1.16.5-server/archive/refs/heads/main.zip";
+const s = "https://github.com/Cube-Devs/cube-valley-1.16.5-client/archive/refs/heads/main.zip";
+const sc = "https://github.com/Cube-Devs/";
 
 function download(url){
     window.open(url);
